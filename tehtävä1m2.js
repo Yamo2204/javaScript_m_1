d0da5299fdee5dacc479b4d6c75a2d5b5ec6503d
Yamo2204 <Yamama.Al-Qaysi@metropolia.fi>
2025-04-05 12:57:05 +0300

    Update tehtävä1m2.js

--- tehtävä1m2.js
+++ tehtävä1m2.js
@@ -1,9 +1,1 @@
-'use strict';
-const number : any[] = [];
-for (let i : number = 0; i < 5; i++) {
-    numbers.push(+prompt(message: 'Enter a number'));
-}
 
-for (let i:number = 4; i >= 0; i--) {
-    console.log(numbers[i]);
-}
